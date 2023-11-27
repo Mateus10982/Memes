1802badc81fb3d8f92f9098c88fa2332ed125df9
Mateus10982 <141938661+Mateus10982@users.noreply.github.com>
2023-11-27 20:02:25 -0300

    Add files via upload

---
+++ src/central/index.jsx
@@ -0,0 +1,11 @@
+import styles from './central.module.css'
+import Rolagem from '../componentes/Rolagem'
+function  Div(){
+  
+    return(
+<>
+<div className={styles.divcentro}><Rolagem /></div>
+</>
+    )
+}
+export default Div
---
+++ src/componentes/cabecalho/index.jsx
@@ -0,0 +1,9 @@
+import styles from "./cabecalho.module.css"
+
+function cabecalho() {
+    return (
+        <header className={styles.header}>MEMES</header>
+    )    
+}
+
+export default cabecalho
