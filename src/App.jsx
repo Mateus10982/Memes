3fa9cbe78ed0279a6e2a9e9420de940f25e70126
Mateus10982 <141938661+Mateus10982@users.noreply.github.com>
2023-11-27 18:50:26 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Rodape from './componentes/rodape/'
+import Cabecalho from './componentes/cabecalho/'
+import Div from './central/'
+function App() {
+  return (
+  <>
+ <Cabecalho />
+ <Div />
+ <Rodape />
+  </>
+  )
+}
+
+export default App
---
+++ src/componentes/rodape/index.jsx
@@ -0,0 +1,10 @@
+import styles from './rodape.module.css'
+function rodape() {
+    return (
+        <footer className={styles.footer}>
+            Feito por Mateus da Silva © 2023
+        </footer>
+    )
+}
+
+export default rodape
