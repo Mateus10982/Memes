6a067f403262881def5e7050fb0c6eedbc5b885d
Mateus10982 <141938661+Mateus10982@users.noreply.github.com>
2023-11-27 20:00:35 -0300

    Delete src directory

--- src/componentes/cabecalho/index.jsx
+++
@@ -1,9 +0,0 @@
-import styles from "./cabecalho.module.css"
-
-function cabecalho() {
-    return (
-        <header className={styles.header}>MEMES</header>
-    )    
-}
-
-export default cabecalho
